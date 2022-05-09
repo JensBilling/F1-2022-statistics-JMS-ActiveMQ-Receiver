4caf492f33ef6fa3a6293a97d91dd054f148b655
Jens Billing <jensis92@hotmail.com>
2022-05-09 19:40:09 +0200

    Prints new racetrack names from JMS message to races.txt file

--- src/main/java/se/iths/f12022statisticsjmsactivemqreceiver/receiver/Receiver.java
+++ src/main/java/se/iths/f12022statisticsjmsactivemqreceiver/receiver/Receiver.java
@@ -6,13 +6,31 @@ import org.springframework.stereotype.Component;
 import se.iths.f12022statisticsjmsactivemqreceiver.config.JmsConfig;
 import se.iths.f12022statisticsjmsactivemqreceiver.model.MessageObject;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 @Component
 public class Receiver {
 
     @JmsListener(destination = JmsConfig.F12022STATS_QUEUE)
     public void listen(@Payload String message
-    ){
-        System.out.println("I got a message.");
-        System.out.println(message);
+    ) throws IOException {
+        System.out.println("I got a JMS message.");
+        System.out.println("Added " + message + " to race database");
+
+        File raceFile = new File("races.txt");
+        Scanner raceScanner = new Scanner(raceFile);
+        String previousRaceData = "";
+        while (raceScanner.hasNext()){
+            previousRaceData += raceScanner.nextLine() + "\n";
+        }
+        raceScanner.close();
+
+        FileWriter writer = new FileWriter("races.txt");
+        writer.write(previousRaceData + "Added " + message + " to race database");
+        writer.close();
+
     }
 }
